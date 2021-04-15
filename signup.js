490507d09408a44af48c756975ba4a602d4a9ab8
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-04-15 17:57:11 +0400

    added sing up page

--- signup.js
+++ signup.js
@@ -1,18 +1,16 @@
-const username = document.getElementById('name')
+const firstName = document.getElementById('first-name')
+const lastName = document.getElementById('last-name')
+const userName = document.getElementById('name')
+const email = document.getElementById('email')
 const password = document.getElementById('password')
+const confirmed = document.getElementById('confirm')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 const signup = document.getElementById('sign-up')
-const login = document.getElementById('login')
-
 
 form.addEventListener('submit', (e) => {
   let messages = []
-  if (username.value === '' || username.value == null) {
-    messages.push('Username is required')
-  }
-
-  if (password.value.length <= 8) {
+  if (password.value.length <= 2) {
     messages.push('Password must be longer than 8 characters')
     clearValue()
   }
@@ -20,27 +18,25 @@ form.addEventListener('submit', (e) => {
     messages.push('Password must be less than 20 characters')
     clearValue()
   }
-
+  if (password.value !== confirmed.value) {
+    messages.push('Password does not match')
+  }
   if (messages.length > 0) {
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
-  }
-  if(messages.length == ''){
+  }else{
     e.preventDefault()
-    login.onclick = log
-    
+    signup.addEventListener('click', () => {
+      window.location.href="./main.html"
+    })
   }
-
 })
 
 function clearValue() {
   password.value = ''
-}
-
-signup.onclick = function () {
-  window.location.href="./signup.html"
+  confirmed.value = ''
 }
 
 
-let log = function () {
-  window.location.href="./main.html"}
+
+

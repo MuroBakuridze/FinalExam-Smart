e7c8e8ad4ffdaafe76c5cb46a4b7ed3b8ea603c4
MuroBakuridze <78916692+MuroBakuridze@users.noreply.github.com>
2021-04-21 20:30:11 +0400

    added Json data

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ let navCallFunc = function () {
 
 
 btn.addEventListener('click', navCallFunc)
+
+
+fetch("https://jsonplaceholder.typicode.com/todos").then(
+  res=>{
+    res.json().then(
+      data=>{
+        console.log(data)
+        if(data.length > 0){
+          var temp = "";
+
+          //...start for loop
+
+          data.forEach((u)=>{
+            temp += "<tr>"
+            temp += "<td>" + u.id+"</td>"
+            temp += "<td>" + u.title+"</td>"
+            temp += "<td>" + u.completed+"</td>"
+          })
+
+          //...close for loop
+          document.getElementById("data").innerHTML = temp;
+        }
+      }
+    )
+  }
+)
